9abb84e4025a69e8ec2ae613c67a15ccf72841cd
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2021-02-09 10:09:04 +0100

    Created packages and fixed configurations

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 import sphinxbootstrap4theme
-
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
